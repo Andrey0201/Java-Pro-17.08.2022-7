917e232ec8fd6c1946b89f8c9e441680d8fbd8e4
Andrii <andriutsa1@ukr.net>
2022-09-10 21:58:40 +0200

    donne all tasks

--- Java-Pro-17.08.2022-7/src/com/javapro/lesson7/Main.java
+++ Java-Pro-17.08.2022-7/src/com/javapro/lesson7/Main.java
@@ -1,6 +1,11 @@
 package com.javapro.lesson7;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    private static String[] words = null;
+
     public static void main(String[] args) {
         findSymbolOccurance("america", 'a');
         System.out.println(findWordPosition("Apollo", "pollo"));
@@ -8,7 +13,8 @@ public class Main {
         System.out.println(stringReverse("Hello"));
         System.out.println(isPalindrome("ERE"));
         System.out.println(isPalindrome("Allo"));
-
+        createWordsArray();
+        guessWord();
     }
 
     public static void findSymbolOccurance(String str, char ch) {
@@ -23,17 +29,49 @@ public class Main {
 
     public static int findWordPosition(String source, String target) {
         return source.indexOf(target);
-
     }
 
     public static String stringReverse(String str) {
         return new StringBuilder(str).reverse().toString();
-
-
     }
 
     public static boolean isPalindrome(String str) {
         return new StringBuilder(str).reverse().toString().equals(str);
+    }
+
+    public static void createWordsArray() {
+        words = new String[]{"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot",
+                "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive",
+                "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+    }
+
+    public static String randomWord() {
+        Random random = new Random();
+        int n = random.nextInt(words.length);
+        return words[n];
+    }
+
+    public static void guessWord() {
+        String word = randomWord();
+        System.out.println("Secret word: " + word);
+        String guess = null;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Guess word. This can be any fruit or any vegetable. ");
+
+        while (!word.equals(guess)) {
+            System.out.println("Enter word:");
+            guess = scanner.next();
+            for (int i = 0; i < 15; i++)
+                if (i < word.length() && i < guess.length() &&
+                        word.charAt(i) == guess.charAt(i)) {
+                    System.out.print(word.charAt(i));
+                } else if (word.equals(guess)) {
+                    break;
+                } else {
+                    System.out.print("#");
+                }
+            System.out.println();
+        }
     }
 }
 
