e69dbf59c69552fd2e2b0c88ec18e9b1f35e6f61
Andrii <andriutsa1@ukr.net>
2022-09-11 15:40:51 +0200

    Fixed NullPointerException for method with input parameter (if String == null)

---
+++ Java-Pro-07.08.2022-8/src/com/javapro/lesson8/Main.java
@@ -0,0 +1,8 @@
+package com.javapro.lesson8;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+  }
+  }
--- Java-Pro-17.08.2022-7/src/com/javapro/lesson7/Main.java
+++ Java-Pro-17.08.2022-7/src/com/javapro/lesson7/Main.java
@@ -4,74 +4,104 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    private static String[] words = null;
 
-    public static void main(String[] args) {
-        findSymbolOccurance("america", 'a');
-        System.out.println(findWordPosition("Apollo", "pollo"));
-        System.out.println(findWordPosition("Apollo", "Plant"));
-        System.out.println(stringReverse("Hello"));
-        System.out.println(isPalindrome("ERE"));
-        System.out.println(isPalindrome("Allo"));
-        createWordsArray();
-        guessWord();
-    }
+  private static String[] words = null;
 
-    public static void findSymbolOccurance(String str, char ch) {
-        char[] chars = str.toCharArray();
-        int count = 0;
-        for (char a : chars)
-            if (a == ch) {
-                count++;
-            }
-        System.out.println(count);
-    }
+  public static void main(String[] args) {
+    findSymbolOccurance(null, 'a');
+    findSymbolOccurance("america", 'b');
+    System.out.println(findWordPosition("Apollo", "pollo"));
+    System.out.println(findWordPosition("Apollo", null));
+    System.out.println(findWordPosition("Apollo", "Plant"));
+    System.out.println(stringReverse("Hello"));
+    System.out.println(stringReverse(null));
+    System.out.println(isPalindrome("ERE"));
+    System.out.println(isPalindrome("Allo"));
+    System.out.println(isPalindrome(null));
+    createWordsArray();
+    guessWord();
+  }
 
-    public static int findWordPosition(String source, String target) {
-        return source.indexOf(target);
-    }
-
-    public static String stringReverse(String str) {
-        return new StringBuilder(str).reverse().toString();
-    }
-
-    public static boolean isPalindrome(String str) {
-        return new StringBuilder(str).reverse().toString().equals(str);
-    }
-
-    public static void createWordsArray() {
-        words = new String[]{"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot",
-                "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive",
-                "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
-    }
-
-    public static String randomWord() {
-        Random random = new Random();
-        int n = random.nextInt(words.length);
-        return words[n];
-    }
-
-    public static void guessWord() {
-        String word = randomWord();
-        System.out.println("Secret word: " + word);
-        String guess = null;
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Guess word. This can be any fruit or any vegetable. ");
-
-        while (!word.equals(guess)) {
-            System.out.println("Enter word:");
-            guess = scanner.next();
-            for (int i = 0; i < 15; i++)
-                if (i < word.length() && i < guess.length() &&
-                        word.charAt(i) == guess.charAt(i)) {
-                    System.out.print(word.charAt(i));
-                } else if (word.equals(guess)) {
-                    break;
-                } else {
-                    System.out.print("#");
-                }
-            System.out.println();
+  public static void findSymbolOccurance(String str, char ch) {
+    if (str != null) {
+      char[] chars = str.toCharArray();
+      int count = 0;
+      for (char a : chars) {
+        if (a == ch) {
+          count++;
         }
+      }
+      System.out.println(count);
+    } else {
+      System.out.println("String is null");
     }
+  }
+
+  public static int findWordPosition(String source, String target) {
+    int position = 0;
+    if (source != null && target != null) {
+      position = source.indexOf(target);
+    } else {
+      System.out.print("String is null ");
+    }
+    return position;
+  }
+
+  public static String stringReverse(String str) {
+    String srtRevers = null;
+    if (str != null) {
+      srtRevers = new StringBuilder(str).reverse().toString();
+    } else {
+      System.out.print("String is ");
+    }
+    return srtRevers;
+  }
+
+  public static boolean isPalindrome(String str) {
+    boolean isPalidrome = false;
+    if (str != null) {
+      isPalidrome = new StringBuilder(str).reverse().toString().equals(str);
+    } else {
+      System.out.print("String is ");
+    }
+    return isPalidrome;
+  }
+
+  public static void createWordsArray() {
+    words = new String[]{"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli",
+        "carrot",
+        "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive",
+        "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+  }
+
+  public static String randomWord() {
+    Random random = new Random();
+    int n = random.nextInt(words.length);
+    return words[n];
+  }
+
+  public static void guessWord() {
+    String word = randomWord();
+    System.out.println("Secret word: " + word);
+    String guess = null;
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Guess word. This can be any fruit or any vegetable. ");
+
+    while (!word.equals(guess)) {
+      System.out.println("Enter word:");
+      guess = scanner.next();
+      for (int i = 0; i < 15; i++) {
+        if (i < word.length() && i < guess.length() &&
+            word.charAt(i) == guess.charAt(i)) {
+          System.out.print(word.charAt(i));
+        } else if (word.equals(guess)) {
+          break;
+        } else {
+          System.out.print("#");
+        }
+      }
+      System.out.println();
+    }
+  }
 }
 
